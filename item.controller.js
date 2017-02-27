d2d3608af6d130bfdce04e5707ad7dcc2a977b32
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-02-27 12:36:11 +0100

    init

---
+++ item.controller.js
@@ -0,0 +1,18 @@
+// CREA UN CONTROLLER PER L'OGGETTO
+angular.module('app').controller('ItemCtrl', function ($scope) {
+
+	$scope.titolo = "Ng-Repeat";
+
+	// CREA UNA LISTA DI OGGETTI ARMA DI TIPO
+	/*
+	 * nome: "Ascia bipenne",
+	 * descrizione: "L’ascia bipenne è una scure a due lame, simbolo del potere minoico.",
+	 * slots : "3",
+	 * image: "http://khazalidgrungron.altervista.org/axes/05.png"
+	 */
+
+	$scope.armi = [];
+
+
+
+})
